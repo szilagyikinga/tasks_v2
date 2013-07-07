95a6fdb44e091aa8b5a26f6aba773c7d4cc6d148
Kinga Szilagyi <szilagyikinga42@gmail.com>
2013-07-07 21:36:41 +0200

    Initialize project

---
+++ src/Tasks/UserBundle/Entity/User.php
@@ -0,0 +1,22 @@
+<?php
+// src/Tasks/UserBundle/Entity/User.php
+
+namespace Tasks\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tasks_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+}
---
+++ src/Tasks/UserBundle/TasksUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tasks\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TasksUserBundle extends Bundle
+{
+}
